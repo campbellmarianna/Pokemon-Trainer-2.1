2c4e308efdb8abb911824c2a639769a93f2d8dc4
Drake Vorndran <DrakeVorndranOffical@gmail.com>
2019-01-30 14:53:17 -0800

    added get pokemon route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const pokemon_trainers = [
     {
     "id": "07832",
     "name": "Susan",
-    "pokemon": ["Nacy", "Drew", "TJ", "Gym", "Tonya"],
+    "pokemon": ["Nacy", "Drew", "TJ", "Gym", "Tonya","Bob"],
     },
 
 ]
@@ -19,8 +19,29 @@ app.get('/', (req, res) => {
   res.json({pokemon_trainers : pokemon_trainers})
 })
 
-app.get('/id', (req, res) => {
-  res.json({pokemon_trainers : pokemon_trainers})
+app.get('/:id', (req, res) => {
+  for(let i = 0; i < pokemon_trainers.length; i++){ //loop through trainers
+    const trainer = pokemon_trainers[i] 
+    if(trainer.id == req.params.id){ //if id is same as trainers
+      return(res.json(trainer)) //return trainer json
+    }
+  }
+  res.json({}) //if no return, return nothing
+})
+
+//get all trainers who have a certin pokemon
+app.get('/pokemans/:name', (req, res) => {
+  let trainers = [] //list of trainers who have the pokemon
+
+  for(let i = 0; i < pokemon_trainers.length; i++){ //loop through trainers
+
+    const trainer = pokemon_trainers[i]
+
+    if(trainer.pokemon.includes(req.params.name)){ //if the trainer has the pokemon
+      trainers.push(trainer) //add it to trainers list
+    }
+  }
+  res.json({trainers: trainers}) //return trainers list
 })
 
 app.listen(3000, () => {
